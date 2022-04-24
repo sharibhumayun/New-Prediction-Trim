a1a106d3cf00a6825d610a19a2431db8259cf0d5
sharibhumayun <66083313+sharibhumayun@users.noreply.github.com>
2022-04-24 12:58:02 +0300

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/sharibhumayun/predict-trim/main/CSAT%20TRIM%20impact%20v2.csv')
